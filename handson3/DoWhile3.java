3a07c958621062284f41f0862bb7d1b94c52a467
jodiy <2381057@unai.edu>
2024-09-12 22:47:55 +0700

    handson3

--- handson3/DoWhile3.java
+++ handson3/DoWhile3.java
@@ -1,4 +1,19 @@
 package handson3;
 
 public class DoWhile3 {
+    public static void main(String[] args) {
+        int counter = 1;
+
+        do{
+            if (counter > 15){
+                break;
+            }
+
+            if (counter % 3 != 0){
+                System.out.println(counter + " ");
+            }
+
+            counter++;
+        }while (counter <= 20);
+    }
 }
